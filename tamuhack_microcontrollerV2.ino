6332f9c2578dd229fb97a2bc1a8d776e1001f627
Juniper Mills <47700808+gblrfgr@users.noreply.github.com>
2024-01-28 04:35:27 -0600

    Added filtering to MCU code

--- tamuhack_microcontrollerV2.ino
+++ tamuhack_microcontrollerV2.ino
@@ -1,125 +1,84 @@
 #ifdef ESP32
-  #include <WiFi.h>
-  #include <HTTPClient.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 #else
-  #include <ESP8266WiFi.h>
-  #include <ESP8266HTTPClient.h>
-  #include <WiFiClient.h>
-  #include <NTPClient.h>
-  #include <WiFiUdp.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
 #endif
 
-//network credentials
-const char* ssid = "TAMU_IoT";
-const char* serverName = "http://plantsaver.peppersalt.info:5000/moisture";
+// network credentials
+const char *ssid = "TAMU_IoT";
+const char *serverName = "http://plantsaver.peppersalt.info:5000/moisture";
 
 String server = "";
 
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org");
+unsigned long long lastUpdate;
 
-float sense_Pin= 0; // Soil Sensor input at Analog PIN A0
-float value= 0;
+float sense_Pin = 0; // Soil Sensor input at Analog PIN A0
+float value = 0;
+float reading = 0;
+float gain = 0.1;
 int count = 0;
 
 const int button = 16;
 int temp = 0;
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(9600);
-  
+
   pinMode(button, INPUT);
 
   WiFi.begin(ssid);
   Serial.println("Connecting");
-  while(WiFi.status() != WL_CONNECTED){
-    delay (500);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
     Serial.print(".");
   }
   Serial.println("");
   Serial.print("Connected to WiFi");
 
-  // Initialize a NTPClient to get time
-  timeClient.begin();
-  
-  }
-
-void loop() {
-  // gets the clock time
-  int time = timeClient.getMinutes();
-  temp = digitalRead(button);
-  //looking to see if its divisible by 30 minutes
-  if (time % 2 == 0){
-    if (count == 0){//gets moisture level from plant
-      count = count + 1;
-      Serial.print("\nMOISTURE LEVEL : ");
-      value= analogRead(sense_Pin);
-      value= (value/1000);
-      
-      //creates the data to send to website
-      String postData = "{\"moisture_level\": " + String(value) + "}";
-    
-      HTTPClient http;
-      WiFiClient wifiClient;
-      http.begin(wifiClient, serverName);
-      http.addHeader("Content-Type", "application/json");
-    
-      int httpCode = http.POST(postData);
-
-      if(httpCode > 0){
-        if(httpCode == HTTP_CODE_OK) {
-          String payload = http.getString();
-          Serial.println(payload);
-        }
-        else{
-          Serial.printf("[HTTP] Get... code %d\n", httpCode);
-        }
-
-        http.end();
-      }
-    }
-    if (temp == HIGH){
-        sendData();
-        delay(1000);
-    }
-  }
-  else{
-    count = 0;
-    if (temp == HIGH){
-        sendData();
-        delay(1000);
-    }
-  }
-
+  lastUpdate = millis();
 }
 
+void loop()
+{
+  temp = digitalRead(button);
+  // looking to see if its divisible by 30 minutes
+  value = analogRead(sense_Pin);
+  value = (value - 5.0) / (870.0 - 5.0);
+  reading = value * gain + reading * (1.0 - gain);
+  if (millis() - lastUpdate >= 1800 || temp == HIGH)
+  {
+    Serial.print("\nMOISTURE LEVEL : ");
 
-void sendData(){
-  Serial.print("\nMOISTURE LEVEL : ");
-      value= analogRead(sense_Pin);
-      value= (value/1000);
-      
+    // creates the data to send to website
+    String postData = "{\"moisture_level\": " + String(reading) + "}";
 
-      //creates the data to send to website
-      String postData = "{\"moisture_level\": " + String(value) + "}";
-    
-      HTTPClient http;
-      WiFiClient wifiClient;
-      http.begin(wifiClient, serverName);
-      http.addHeader("Content-Type", "application/json");
-    
-      int httpCode = http.POST(postData);
+    HTTPClient http;
+    WiFiClient wifiClient;
+    http.begin(wifiClient, serverName);
+    http.addHeader("Content-Type", "application/json");
 
-      if(httpCode > 0){
-        if(httpCode == HTTP_CODE_OK) {
-          String payload = http.getString();
-          Serial.println(payload);
-        }
-        else{
-          Serial.printf("[HTTP] Get... code %d\n", httpCode);
-        }
+    int httpCode = http.POST(postData);
 
-        http.end();
+    if (httpCode > 0)
+    {
+      if (httpCode == HTTP_CODE_OK)
+      {
+        String payload = http.getString();
+        Serial.println(payload);
       }
+      else
+      {
+        Serial.printf("[HTTP] Get... code %d\n", httpCode);
+      }
+
+      http.end();
+    }
+    lastUpdate = millis();
+  }
 }
